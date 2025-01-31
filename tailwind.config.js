0ac81d4c21cef89307875ecb7241715aa87af90b
Tamim <jobaertamim7@gmail.com>
2025-01-31 16:30:19 +0600

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    theme: {
+        extend:{}
+    },
+    plugins: [
+        require('daisyui'),
+        require("@tailwindcss/forms"), require("@tailwindcss/typography"),
+    ],
+        
+  }
